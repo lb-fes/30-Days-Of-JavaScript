2fa2a793a367f528abe662e97358b620023bbc36
lb-fes <51531901+lb-fes@users.noreply.github.com>
2022-12-05 21:45:29 +0800

    Complete Introduction

---
+++ CN/Day01_Introduction/dataTypes.js
@@ -0,0 +1,14 @@
+// undefined
+let firstName
+console.log(firstName)
+
+// null
+let emptyValue = null
+console.log(emptyValue)
+
+// 核对数据类型
+console.log(typeof 'Asabeneh')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ CN/Day01_Introduction/variables.js
@@ -0,0 +1,14 @@
+// 声明不同数据类型的不同变量
+let FirstName = 'l'
+let lastName = 'b'
+let country = 'China'
+let city = 'Beijing'
+let age = 100
+let isMarried = false
+console.log(FirstName, lastName, country, city, age, isMarried)
+
+// 声明静态数字变量
+const gravity = 9.81
+const boilingPoint = 100
+const PI = 3.14
+console.log(gravity, boilingPoint, PI)
